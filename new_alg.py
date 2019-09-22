27800941bebb8eee5fcfcb0d5dcb658d5465eebd
Pashs-ba <pashs.ba@gmail.com>
2019-09-22 20:57:19 +0300

    module_struct

--- new_alg.py
+++ new_alg.py
@@ -2,5 +2,7 @@ import random
 raw_matrix = [[0 for i in range(5)] for j in range(5)]
 raw_matrix[0][0] = 1
 raw_matrix[random.randint(1, 4)][random.randint(1, 4)] = 1
-for i in raw_matrix:
-    print(i)
+if __name__ == '__main__':
+    for i in raw_matrix:
+        print(i)
+
