3a9f07dc2c3657a62d976d562f2da7c9860734b2
Pashs-ba <pashs.ba@gmail.com>
2019-09-24 08:29:01 +0300

    some испраление oshibok number dva

--- new_alg.py
+++ new_alg.py
@@ -1,6 +1,6 @@
 import random
 
-SIZE = 101
+SIZE = 21
 # делаем матрицу
 raw_matrix = [[0 for i in range(SIZE)] for j in range(SIZE)]
 raw_matrix[0][0] = 1
@@ -9,52 +9,72 @@ x = random.randint(1, SIZE-1)
 y = random.randint(1, SIZE-1)
 # начальная точка
 raw_matrix[y][x] = 1
-while True:
-    a = random.randint(1, 4)  # 1 - север, 2 - юг, 3 - восток, 4 - запад
+b = 0
+good = [1, 2, 3, 4]
+choise = [1, 2, 3, 4]
+while b != 4:
+    a = random.choice = choise  # 1 - север, 2 - юг, 3 - восток, 4 - запад
     if a == 1:
-        if y-2 < 0 or y-2 > SIZE or raw_matrix[y-2][x] == 1:  # проверка на наличие прохода и выходы за пределы листа
-            pass
+        if y-2 < 0 or y-2 > SIZE-1 or raw_matrix[y-2][x] == 1:  # проверка на наличие прохода и выходы за пределы листа
+            b += 1
+            print('not')
+            choise.pop(1)
+            continue
+
+
         else:
             raw_matrix[y-1][x] = 1
             y -= 2
             raw_matrix[y][x] = 1
+            b = 0
         for i in raw_matrix:
             print(i)
         print("  ")
 
     if a == 2:
-        if y+2 < 0 or y+2 > SIZE or raw_matrix[y+2][x] == 1:  # проверка на наличие прохода и выходы за пределы листа
-            pass
+        if y+2 < 0 or y+2 > SIZE-1 or raw_matrix[y+2][x] == 1:  # проверка на наличие прохода и выходы за пределы листа
+            b += 1
+            print('not')
+            choise.pop(2)
+            continue
         else:
             raw_matrix[y+1][x] = 1
             y += 2
             raw_matrix[y][x] = 1
+            b = 0
         for i in raw_matrix:
             print(i)
         print("  ")
 
     if a == 3:
-        if x-2 < 0 or x-2 > SIZE or raw_matrix[y][x-2] == 1:  # проверка на наличие прохода и выходы за пределы листа
-            pass
+        if x-2 < 0 or x-2 > SIZE-1 or raw_matrix[y][x-2] == 1:  # проверка на наличие прохода и выходы за пределы листа
+            b += 1
+            print('not')
+            continue
         else:
+
             raw_matrix[y][x-1] = 1
             x -= 2
             raw_matrix[y][x] = 1
+            b = 0
         for i in raw_matrix:
             print(i)
         print("  ")
 
     if a == 4:
-        if x+2 < 0 or x+2 > SIZE or raw_matrix[y][x+2] == 1:  # проверка на наличие прохода и выходы за пределы листа
-            pass
+        if x+2 < 0 or x+2 > SIZE-1 or raw_matrix[y][x+2] == 1:  # проверка на наличие прохода и выходы за пределы листа
+            print('not')
+            b += 1
+            continue
         else:
             raw_matrix[y][x+1] = 1
             x += 2
             raw_matrix[y][x] = 1
+            b = 0
         for i in raw_matrix:
             print(i)
         print("  ")
-
+print(b)
 
 
 # if __name__ == '__main__':
