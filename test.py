66098088879df91cfb3cd445cf71f11e8de18325
Pashs-ba <pashs.ba@gmail.com>
2019-09-25 20:53:32 +0300

    delete vse k xrenam

--- test.py
+++
@@ -1,9 +0,0 @@
-import random
-SIZE = 21
-raw_matrix = [[0 for i in range(SIZE+1)] for j in range(SIZE+1)]
-def point_two():
-    x = random.randint(1, SIZE)
-    y = random.randint(1, SIZE)
-    return x, y
-
-
